3524f838a6b7760150612c530a2ea9870c80aebc
Oleksandr Chernomorets <achernomorez@gmail.com>
2023-03-02 15:42:37 +0300

    axios

--- src/Services/fetchApi.js
+++ src/Services/fetchApi.js
@@ -1,51 +1,59 @@
 import axios from 'axios';
 
-const API_KEY = '2d47787539d4eda04ce92011fd824a06';
-const BASE_URL = 'https://api.themoviedb.org/3';
-axios.defaults.baseURL = BASE_URL;
+const KEY = '2d47787539d4eda04ce92011fd824a06';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-const END_POINTS = {
-  trending: '/trending/movie/day',
-  querySearch: '/search/movie',
-  movieDetails: '/movie',
-  movieCredits: '/credits',
-  movieReviews: '/reviews',
+export const getMovies = async () => {
+  const { data } = await axios('/trending/movie/day', {
+    params: {
+      api_key: KEY,
+    },
+  });
+
+  return data.results;
 };
 
-export const getMovies = async (page = 1) => {
-  const resp = await axios.get(
-    `${END_POINTS.trending}?api_key=${API_KEY}&language=en-US&include_adult=false`
-  );
-  return resp.data.results;
+export const getMovieDetails = async movieId => {
+  const { data } = await axios(`/movie/${movieId}`, {
+    params: {
+      api_key: KEY,
+    },
+  });
+
+  return data;
 };
 
-export const getMovieDetails = async id => {
-  const resp = await axios.get(
-    `${END_POINTS.movieDetails}/${id}?api_key=${API_KEY}&language=en-US`
-  );
-  return resp.data;
+export const fetchCredits = async movieId => {
+  const { data } = await axios(`/movie/${movieId}/credits`, {
+    params: {
+      api_key: KEY,
+    },
+  });
+
+  return data.cast;
 };
 
-export const fetchByQuery = async (query, page = 1) => {
-  const resp = await axios.get(
-    `${END_POINTS.querySearch}?api_key=${API_KEY}&page=${page}&query=${query}&language=en-US&include_adult=false`
-  );
-  return resp.data.results;
+export const fetchReviews = async movieId => {
+  const { data } = await axios(`/movie/${movieId}/reviews`, {
+    params: {
+      api_key: KEY,
+    },
+  });
+
+  return data.results;
 };
 
-export const fetchReviews = async (id, page = 1) => {
-  const resp = await axios.get(
-    `${END_POINTS.movieDetails}/${id}/${END_POINTS.movieReviews}?api_key=${API_KEY}&language=en-US&page=${page}`
-  );
-  return resp.data.results;
+export const fetchByQuery = async query => {
+  const { data } = await axios('/search/movie', {
+    params: {
+      api_key: KEY,
+      include_adult: false,
+      query,
+    },
+  });
+
+  return data.results;
 };
 
-export const fetchCredits = async id => {
-  const resp = await axios.get(
-    `movie/${id}${END_POINTS.movieCredits}?api_key=${API_KEY}&language=en-US`
-  );
-  return resp.data.cast;
-};
-
-
+export const startImageUrl = 'https://image.tmdb.org/t/p/w500';
 
